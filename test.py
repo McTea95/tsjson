71f5bf09803358c820448ad23ce82875fa180a91
McTea95 <thomas.kirsch@yahoo.de>
2023-01-05 12:52:50 +0100

    Aufgabe 1 und 2

--- test.py
+++ test.py
@@ -1,1 +1,46 @@
-print("hallo")
+# Python program to read
+# json file
+  
+  
+import json
+  
+# Opening JSON file
+f = open('tsjson/accounts.json')
+  
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+
+# Iterating through the json
+# list
+#for i in data:
+#    print(i)
+
+
+while True:
+    eingabe = (input("Bitte gebe alter oder Kontostand ein:\nquit um abzubrechen.\n"))
+    if eingabe == "quit":
+        print("Programm wird beendet...")
+        break
+    elif eingabe == "alter":
+        for i in data:
+            if i["age"] > 25:
+                print(i["name"],"ist",i["age"],"Jahre alt.")    
+    elif eingabe == "Kontostand":
+        balance = [] #helping array data, filled from the json file
+
+        for i in data: 
+            balance.append(i["balance"]) #append the json file into the array
+
+        highst = balance[0] #helping variblg to save the highst value
+
+        for number in balance: #sorting
+            if number > highst:
+                highst = number
+        print(highst)
+    else:
+        print("falsche Eingabe, versuche es erneut!")
+
+
+# Closing file
+f.close()
